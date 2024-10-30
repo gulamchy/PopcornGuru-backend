f5afe6d742ac3097e0b0eb4f00e69c13a842fa81
gulamchy <gulamsulaman@gmail.com>
2024-10-30 13:01:17 -0700

    Fixing the index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://popcorn-guru-frontend.vercel.app",
+    origin: process.env.FRONTEND_URL,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
